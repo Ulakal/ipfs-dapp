d3cc501d377f43566f9cdd3f71cac9fe1f53c2bb
Ula Kalinowska <ursz.kalinowska@gmail.com>
2020-12-09 20:15:41 +0100

    3. Frontend connection with blockchain

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3';
+import Meme from '../abis/Meme.json'
 
 const ipfsClient = require('ipfs-http-client');
 const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
 
 class App extends Component {
 
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
   constructor(props) {
     super(props);
     this.state = {
+      account: '',
+      contract: '',
       buffer: null,
-      memeHash: 'QmQU6qcmRQkr4e5hdyPf6iL8g821xY6NfaZzNugwiWJehY'
+      memeHash: ''
+    }
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } 
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const networkId = await web3.eth.net.getId()
+    const networkData = Meme.networks[networkId]
+    
+    if(networkData) {
+      const contract = await web3.eth.Contract(Meme.abi, networkData.address)
+      this.setState({ contract })
+      const memeHash = await contract.methods.memeHash().call()
+      this.setState({ memeHash })
+    } else {
+      window.alert("contract not deployed to detected network")
     }
   }
 
@@ -29,10 +66,15 @@ class App extends Component {
   onSubmit = (event) => {
     event.preventDefault()
     
-    ipfs.add(this.state.buffer, (error, result) => {
+    ipfs.add(this.state.buffer, async (error, result) => {
       console.log("ipfs result: ", result)
       const memeHash = result[0].hash 
-      this.setState({ memeHash })
+      await this.state.contract.methods.set(memeHash).send({from: this.state.account})
+      .then((receipt) => {
+        console.log(receipt)
+        return this.setState({ memeHash })
+      })
+      
       if (error) {
         console.error(error)
         return
